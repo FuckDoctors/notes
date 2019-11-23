f2a55876337e780b1144bd0f1d87d29338a01e46
XiaoNa <xiaona@XiaoNadeMBP.newifi3>
2019-11-24 02:10:16 +0800

    添加circleci

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -e
+
+# yarn docs:build
+
+git config --global user.email "zhbchwin@163.com"
+git config --global user.name "FuckDoctors"
+
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m "Deploying docs at $(date "+%Y-%m-%d %H:%M:%S")"
+
+git push -f git@github.com:FuckDoctors/learning-notes.git master:gh-pages
+
+cd -
+rm -rf docs/.vuepress/dist
