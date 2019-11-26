80a18b1d3d2b9f117a3ff77830fa76cd283db941
zhaobc <zhbchwin@163.com>
2019-11-26 19:16:59 +0800

    使用postcss-import后，代码高亮无效，因为@import出了问题。
    @vuepress/theme-default/styles/code.styl
    137:8   ‼  @import must precede all other statements (besides @charset)

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ module.exports = {
   plugins: [
     /* You should remove 'autoprefixer' from your postcss plugin list since it's already included by postcss-cssnext. */
     // require('autoprefixer'),
+    // require('postcss-easy-import')({
+    //   extensions: ['.css', '.scss', '.styl']
+    // }),
     require('postcss-import')({
       plugins: [
         require('stylelint')({
