ff35de233b12fca9cf089b59dde2f8753b1be0e0
FuckDoctors <zhbchwin@163.com>
2019-12-06 22:16:26 +0800

    feat: 暂存iview的CodeDemo，未调试

---
+++ docs/.vuepress/components/CodeDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <Demo />
+</template>
+
+<script>
+import Demo from './demo/Demo.vue';
+
+export default {
+  name: 'CodeDemo',
+  components: {
+    Demo
+  }
+};
+</script>
---
+++ docs/.vuepress/components/demo/Demo.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- https://github.com/view-design/weapp-doc/blob/master/src/components/demo.vue -->
+  <div class="demo demo-block">
+    <div class="demo-case">
+      <div class="result">Result</div>
+      <div class="header">header</div>
+      <div class="desc">desc</div>
+    </div>
+    <div class="split"></div>
+    <div class="demo-code">
+      <div class="code-container">
+        <div class="code-content">xxxx</div>
+        <div class="icons">
+          <span class="btn">Btn1</span>
+          <span class="btn">Btn2</span>
+          <span class="btn">Btn3</span>
+        </div>
+      </div>
+      <div class="code-more"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo'
+};
+</script>
+
+<style lang="postcss">
+@import './demo.pcss';
+</style>
